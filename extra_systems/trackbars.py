f3337225151720303fbf283e55e81b5afe4798b2
muhammedemiraysan <89260017+muhammedemiraysan@users.noreply.github.com>
2022-08-03 23:19:40 +0300

    Update trackbars.py

--- extra_systems/trackbars.py
+++ extra_systems/trackbars.py
@@ -2,16 +2,28 @@ import cv2
 import numpy
 class Trackbars():
     def __init__(self):
+        
+        self.trackbar_R = 0
+        
         super().__init__()
-    def create_trackbars(self):
-        def nothing():
-            pass
-        self.img = numpy.zeros((300, 512, 3), numpy.uint8)
-        cv2.namedWindow('trackbarWindow')
-        cv2.createTrackbar('R', 'trackbarWindow', 0, 255, nothing)
 
-    def get_trackbar_pos(self):
+    def create_trackbars(self):
+        
+        def nothing(*args):
+            pass
+        
+        self.img = numpy.zeros((300, 512, 3), numpy.uint8)
+        
+        cv2.namedWindow('trackbarWindow')
+
+        cv2.createTrackbar('Width', 'trackbarWindow', 0, 100, nothing)
+        cv2.createTrackbar('Height', 'trackbarWindow', 0, 100, nothing)
+        cv2.createTrackbar('lines', 'trackbarWindow', 0, 1, nothing)
+
+        cv2.setTrackbarPos('Width', 'trackbarWindow', 20)
+        cv2.setTrackbarPos('Height', 'trackbarWindow', 20)
+        cv2.setTrackbarPos('lines', 'trackbarWindow', 1)
+    def trackbar_screen(self):
         cv2.imshow('trackbarWindow', self.img)
         if cv2.waitKey(1) == 27:
             exit()        
-        print(cv2.getTrackbarPos('R', 'trackbarWindow'))
